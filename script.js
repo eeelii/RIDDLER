2878f9888cc294699d3200cad53ccade07dc17f2
eeelii <eliseocutz95@gmail.com>
2024-04-27 09:42:24 -0400

    adjusted user interactive

--- script.js
+++ script.js
@@ -46,30 +46,42 @@ const riddles = [
 console.log(riddles[0].question);
 console.log(riddles[0].answer);
 
-// for (let i = 0; i < riddles.length; i++) {
-//   console.log(`Riddle ${i + 1}: ${riddles[i].question}`);
-//   console.log(`Answer ${i + 1}: ${riddles[i].answer}`);
-// }
 // create a random number store into a variable 
 const ranNum = Math.floor(Math.random() * riddles.length);
 // get the btn to start the game 
 const btn = document.getElementById("Ready");
 // get the users input value 
-const userInput = document.getElementById("user-value").value;
+const userInput = document.getElementById("user-value");
+// get the score element 
+const score = document.getElementById("user-Score");
 // set a counter
 counter = 0;
 // create a function that displays a question on the DOM 
 function displayQuestion() {
   const riddleQ = document.querySelector(".lstRiddle");
-  btn.innerHTML = "Next";
-  // display each question using a counter
+  // display the first riddle 
   riddleQ.innerHTML = riddles[counter].question;
-  counter++;
-  // check if the counter = to the length of riddles and start again
-  if (counter === riddles.length){
-    counter = 0;
+  // change btn to next
+  // btn.innerHTML = "Next";
+}
+
+// create a function that checks if the answer for the riddle is correct 
+function answerCheck() {
+  if (userInput.value === "") {
+    btn.innerHTML = "Must Answer First";
+  } else if (userInput.value === riddles[counter].answer) {
+    btn.innerHTML = "Next";
+    counter++;
+    if (counter === riddles.length) {
+      counter = 0;
+    }
+    displayQuestion();
   }
 }
 
-btn.onclick = displayQuestion;
+// get the submit element 
+const submit = document.getElementById("submit");
 
+submit.onclick = answerCheck;
+// dispaly next question 
+btn.onclick = displayQuestion;
